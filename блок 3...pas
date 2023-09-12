0e2ad35491a5b9b53a0fb3e8eaa0bf2d84e3a17a
valeriyorlo <144387877+valeriyorlo@users.noreply.github.com>
2023-09-12 17:11:37 +0300

    Add files via upload

---
+++ блок 3...pas
@@ -0,0 +1,15 @@
+﻿program block3zad3;
+var N1, N2, N3, Res:integer;
+begin 
+  write('Введите первое целое число');
+  readln(N1);
+  write('Введите второе целое число');
+  readln(N2);
+  write('Введите третье целое число');
+  readln(N3);
+  Res:=0;
+  if N1>0 then inc(Res);
+  if N2>0 then inc(Res);
+  if N3>0 then Inc(Res);
+  writeln(Res);
+end.
---
+++ блок 3.pas
@@ -0,0 +1,10 @@
+﻿program block3zad1;
+var a, b, c, min: integer;
+begin
+  writeln('Введите три числа');
+ readln(a, b, c);
+  min:=a;
+  if b<min then min:=b;
+  if c<min then min:=c;
+  writeln(min);
+end.
